5ae76761029d4063595aa1abded822895918bae8
Felipe Buche <felipe.buche.tds2023@gmail.com>
2025-05-25 14:33:46 -0300

    Installing Node.Js and TailwindCSS

---
+++ app/view/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="assets\img\icon\ICONE BRANCO.ico">
+
+    <script src="../../tailwind.config.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+    <title>Bypass</title>
+</head>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+<?php include('app/view/head.php') ?>
+
+<body class="overflow-clip bg-preto">
+    <header class="bg-branco h-40 w-full p-10">
+        <img src="assets\img\LOGO COLORIDA.png" alt="" class="h-10 w-auto">
+    </header>
+
+    <section>
+        teste
+    </section>
 </body>
 </html>
