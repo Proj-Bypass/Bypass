8c50c6b56900702d46d9d7fbf944392e1b362993
Felipe Buche <felipe.buche.tds2023@gmail.com>
2025-05-25 15:44:24 -0300

    Tailwind and basic html configuration

--- app/view/head.php
+++ app/view/head.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <meta http-equiv="refresh" content="5">
+
     <link rel="shortcut icon" href="assets\img\icon\ICONE BRANCO.ico">
 
-    <script src="../../tailwind.config.js"></script>
+    <link rel="stylesheet" href="C:\xampp\htdocs\Bypass\assets\css\style.css">
 
+    <link href="./output.css" rel="stylesheet">
+    
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
     <title>Bypass</title>
--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php include('app/view/head.php') ?>
 
-<body class="overflow-clip bg-preto">
-    <header class="bg-branco h-40 w-full p-10">
-        <img src="assets\img\LOGO COLORIDA.png" alt="" class="h-10 w-auto">
+<body class="overflow-clip bg-[#1f1f20]">
+    <header class="h-35 w-full bg-[#f6f1f6]">
+        <div class="container mx-auto flex justify-between items-center font-bold text-2xl">
+
+            <a href="">teste</a>
+            <a href="">teste2</a>
+
+            <a href="index.php"><img src="assets\img\LOGO COLORIDA.png" alt="" class="h-40 w-auto"></a>
+
+            <a href="">teste3</a>
+            <a href="">teste4</a>
+        </div>
+        
     </header>
 
     <section>
-        teste
+
     </section>
 </body>
+
 </html>
