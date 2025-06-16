0e34c4589e022f8138521f4d887983094c63a0fb
Felipe Buche <felipe.buche.tds2023@gmail.com>
2025-06-16 19:43:32 -0300

    .

--- index.php
+++ index.php
@@ -4,19 +4,19 @@
     <header class="h-35 w-full bg-[#f6f1f6]">
         <div class="container mx-auto flex justify-between items-center font-bold text-2xl">
 
-            <a href="">teste</a>
-            <a href="">teste2</a>
+            <a href="">Sobre o projeto</a>
+            <a href="">Membros</a>
 
             <a href="index.php"><img src="assets\img\LOGO COLORIDA.png" alt="" class="h-40 w-auto"></a>
 
-            <a href="">teste3</a>
-            <a href="">teste4</a>
+            <a href="">Entrar</a>
+            <a href="">Cadastrar</a>
         </div>
         
     </header>
 
     <section>
-
+        <h1>TESTE</h1>
     </section>
 </body>
 
